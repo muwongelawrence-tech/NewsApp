efae8ecd90c8126dc0f7586d9825ee9b3ff3d2ed
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-04-12 02:00:33 +0300

    replacing img with Image

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import Image from "next/image";
 
 function Avatar({ url, className }) {
   return (
-    <img 
-     loading='lazy'
-     src = { url || "/ml.jpg" }
-     className= {`h-10 rounded-full  cursor-pointer  
-     transition duration-150 transform hover:scale-110 ${ className }`}
-     alt = "profile pic"
-    />
+
+    <Image
+       src = {url || "/ml.jpg"}
+        height = { 60 }
+        width = { 60 }
+       loading ="lazy"
+       objectFit ="cover"
+       className= {`h-10 rounded-full  cursor-pointer  
+          transition duration-150 transform hover:scale-110 ${ className }`}
+        alt = "profile pic"
+     />
+
+    // <img 
+    //  loading='lazy'
+    //  src = { url || "/ml.jpg" }
+    //  className= {`h-10 rounded-full  cursor-pointer  
+    //  transition duration-150 transform hover:scale-110 ${ className }`}
+    //  alt = "profile pic"
+    // />
 
 
   );
