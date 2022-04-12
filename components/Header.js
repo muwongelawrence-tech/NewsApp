4b76d999d2a3d37e4b4c91548a2de789eca18f6f
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-04-12 03:06:09 +0300

    removing the svg problem

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 
 import  Image  from 'next/image';
 import React, { useState } from 'react';
+import { MenuIcon } from "@heroicons/react/outline";
 import Avatar from './Avatar';
 import { signIn , signOut , useSession } from "next-auth/client";
 
@@ -123,9 +124,7 @@ function Header( { onChange }) {
                 className=' sm:hidden border-2 border-blue-400 rounded text-blue-500 hover:text-blue-300 active:text-blue-300 '
                 onClick ={ collapse }
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                  </svg>
+                  <MenuIcon className = "h-6 " />
                 </div>
 
               { showmenu && (
